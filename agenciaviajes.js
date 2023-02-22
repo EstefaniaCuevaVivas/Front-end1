c348ebfb89650574338ab79afb2f43e467166253
Estefania-Cueva <estefaniacuevavivas@gmail.com>
2023-02-22 11:07:21 +0100

    todo

--- agenciaviajes.js
+++ agenciaviajes.js
@@ -23,12 +23,28 @@ function informacion(){
 
 }
 
+
+document.getElementById("acumulados").innerHTML = acumulado;
+
 function filtrarDatos(){
-  for(let i = 0 ; i<datos.length;i++){
-    if(datos[i].destino.toUpperCase() == "CANARIAS" || datos[i].destino.toUpperCase() == "MALLORCA" || datos[i].destino.toUpperCase() == "GALICIA" ){
-      console.log(datos[i])
-    }
+  let filtrado = "";   
+    
+  for(let i = 0; i<datos.length; i++){ 
+
+    if (datos[i].destino.toUpperCase() == "CANARIAS" || datos[i].destino.toUpperCase() == "MALLORCA"  
+    || datos[i].destino.toUpperCase() == "GALICIA"){    
+
+      console.log(datos[i])  
+
+      filtrado += `<p>Nombre: ${datos[i].nombre}</p>             
+      <p>Origen: ${datos[i].origen}</p>             
+      <p>Destino: ${datos[i].destino}</p>             
+      <p>Número de personas: ${datos[i].personas}</p>             
+      <p>Fecha: ${datos[i].fecha}</p>  `                  
+    }    
+
   }
+
+document.getElementById("mostrardatos").innerHTML = `${filtrado}`;
+
 }
-
-
